754176673f21b37305df3a063d30337998abf1ee
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-14 10:38:43 +0900

    C#

---
+++ MxComponentTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace MxComponentTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(153, 102);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "열기";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(12, 120);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(153, 102);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "닫기";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(12, 228);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(153, 102);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "읽기";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(12, 336);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(153, 102);
+            this.btnWrite.TabIndex = 3;
+            this.btnWrite.Text = "쓰기";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnWrite;
+    }
+}
+
---
+++ MxComponentTest/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MxComponentTest
+{
+    public partial class Form1 : Form
+    {
+        ActUtlType64Lib.ActUtlType64 MxComponent;
+        public Form1()
+        {
+            InitializeComponent();
+            MxComponent = new ActUtlType64Lib.ActUtlType64();
+        }
+
+        void MxOpen()
+        {
+            int iReturnCode;
+
+            MxComponent.ActLogicalStationNumber = 0;
+            iReturnCode = MxComponent.Open();
+        }
+        void MxClose()
+        {
+            int iReturnCode;
+
+            iReturnCode = MxComponent.Close();
+        }
+
+        short ReadDeviceOne(string name)
+        {
+            int iReturnCode;
+            short deviceValue;
+
+            iReturnCode = MxComponent.ReadDeviceRandom2(name, 1, out deviceValue);
+            return deviceValue;
+        }
+
+        void WriteDeviceOne(string name, short value)
+        {
+            int iReturnCode;
+
+            iReturnCode = MxComponent.WriteDeviceRandom2(name, 1, value);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            MxOpen();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            MxClose();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            short retValue;
+            retValue = ReadDeviceOne("M100");
+
+            MessageBox.Show(retValue.ToString());
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            WriteDeviceOne("M100", 1);
+        }
+    }
+}
