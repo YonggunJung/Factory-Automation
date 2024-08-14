5245a49ff64c2685b895c11906920aabc1bc2b74
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-14 10:40:07 +0900

    C#

---
+++ BarcodeReaderTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//바코드 리더기가 필요하지만 없이 그냥 공부함
+namespace BarcodeReaderTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("<T/>");
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //textBox1.AppendText(serialPort1.ReadLine());  //에러남
+            //WriteText(serialPort1.ReadLine());            //에러남
+            string sendStr = serialPort1.ReadLine() + "\n";
+            BeginInvoke(new WriteTextCallback(WriteText), new object[] { sendStr });
+        }
+
+        private void WriteText(string str)
+        {
+            this.textBox1.AppendText(str);
+        }
+
+        delegate void WriteTextCallback(string str);
+    }
+}
---
+++ BarcodeReaderTest2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarcodeReaderTest2
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        NetworkStream stream;
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new TcpClient();
+            client.Connect("192.168.100.1", 2000);
+            stream = client.GetStream();
+
+            // 쓰레드 기본 문법. 익혀 두면 좋음
+            Thread thread = new Thread(GetMessage);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        ~Form1()
+        {
+            stream.Close();
+            client.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes("<T/>");
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private void GetMessage()
+        {
+            // 혼자 노는 함수.
+            while (true)
+            {
+                int size = client.ReceiveBufferSize;
+                byte[] buffer = new byte[size];
+                int bytes = stream.Read(buffer, 0, buffer.Length);
+                string sendStr = Encoding.ASCII.GetString(buffer, 0, bytes) + "\n";
+
+                BeginInvoke(new WriteTextCallback(WriteText), new object[] { sendStr });
+            }
+        }
+
+        private void WriteText(string str)
+        {
+            this.textBox1.AppendText(str);
+        }
+
+        delegate void WriteTextCallback(string str);
+    }
+}
