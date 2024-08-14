1f653acca27e858ba677014f82c23e43483300e4
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-08-14 10:40:47 +0900

    C#

---
+++ TrafficLight/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficLight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Thread thread = new Thread(SignalTower);
+            thread.Start();
+        }
+
+        void SignalTower()
+        {
+            int D100 = 0;
+            while (true)
+            {
+                switch (D100)
+                {
+                    case 0:
+                        LampRed.BackColor = Color.Red;
+                        LampYellow.BackColor = SystemColors.Control;
+                        LampGreen.BackColor = SystemColors.Control;
+                        Thread.Sleep(3000);
+                        D100 = 100;
+                        break;
+
+                    case 100:
+                        LampRed.BackColor = SystemColors.Control; 
+                        LampYellow.BackColor = Color.Yellow;
+                        LampGreen.BackColor = SystemColors.Control;
+                        Thread.Sleep(3000);
+                        D100 = 200;
+                        break;
+
+                    case 200:
+                        LampRed.BackColor = SystemColors.Control;
+                        LampYellow.BackColor = SystemColors.Control;
+                        LampGreen.BackColor = Color.Green;
+                        Thread.Sleep(3000);
+                        D100 = 0;
+                        break;
+                }
+                
+            }
+        }
+    }
+}
